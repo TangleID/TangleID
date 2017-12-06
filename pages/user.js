aba7203b5ea2a16902772984c83d2fb48f9ef4c6
abalone0204 <abalone0204@gmail.com>
2017-12-06 15:02:57 +0800

    Basic User page setup

--- pages/user.js
+++ pages/user.js
@@ -1,16 +1,35 @@
+import withRedux from 'next-redux-wrapper'
+import configureStore from '../store/configureStore'
+import checkTangleUsers from '../actions/checkTangleUsers'
+import fetchOffTangleUserList from '../actions/fetchOffTangleUserList'
 import Layout from '../layouts/Main'
 
 const UserPage = (props) => {
-	console.log('props: ', props)
+	const { user } = props
 	return (
 		<Layout>
-			<h2>User</h2>
+			<h2>{user.claim.firstName}</h2>
 		</Layout>
 	)
 }
 
 UserPage.getInitialProps = async (context) => {
-	return {}
+	const { store } = context
+	const { id } = context.query
+	await store.dispatch(fetchOffTangleUserList())
+	await store.dispatch(checkTangleUsers([{ id }]))
+	return { id }
 }
 
-export default UserPage
+const mapStateToProps = (state, ownProps) => {
+	const { id } = ownProps
+	const { users } = state
+	const { offTangleData, validData } = users
+	const validIds = validData.map(v => v.id)
+	// TODO: Use selector to get better performance
+	const userList = offTangleData.filter(d => validIds.indexOf(d.id) !== -1)
+	const user = userList.find(u => u.id === id)
+	return { user }
+}
+
+export default withRedux(configureStore, mapStateToProps)(UserPage)
