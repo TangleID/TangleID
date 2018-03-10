3f258f39e965e835b851d335d9c5694fea9cc84a
jkrvivian <jkrvivian@gmail.com>
2018-03-10 14:46:28 +0800

    Add ReactJS component ClaimList
    ClaimList is a href list showing all claims of a user.

---
+++ components/ClaimList/index.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+const ClaimList = (props) => {
+	const { claims } = props
+        console.log(claims)
+	return (
+		<div>
+			{claims.map(claim => (
+				<div key={`claim-${claim}`}>
+					<Link href={`/claims/info/?hash_txn=${JSON.stringify(claim)}`} as={`/claims/info/${claim}`}>
+						<a>{claim}</a>
+					</Link>
+				</div>
+			))}
+		</div>
+	)
+}
+
+export default ClaimList 
