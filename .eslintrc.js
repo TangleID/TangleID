50916ca2e8dfbff8cc3a114fc9e068cb30c87e2b
dadachan <vincent8397@gmail.com>
2018-04-02 11:01:38 +0800

    Use ES7 eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,45 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true,
-        "commonjs": true,
-        "es6": true
+	"parser": "babel-eslint",
+  "env": {
+    "node": true,
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": "2017",
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "2017",
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      "tab"
     ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "no-console": [
-            "error",
-            { allow: [ "log", "warn", "error" ] }
-        ],
-        "react/jsx-uses-vars": 2,
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
-    }
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-console": [
+      "error",
+      { allow: [ "log", "warn", "error" ] }
+    ],
+    "react/jsx-uses-vars": 2,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+  }
 };
