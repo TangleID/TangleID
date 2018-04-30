f2e41521c7cc23724ded95cea7f5414ad5925462
HexRabbit <mikuqb8718@gmail.com>
2018-04-30 13:28:35 +0800

    Change encrypt key length to 2048

--- utils/createKeyPair.js
+++ utils/createKeyPair.js
@@ -1,7 +1,7 @@
 const ursa = require('ursa')
 
 module.exports = function createKeyPair() {
-	const key = ursa.generatePrivateKey(1024)
+	const key = ursa.generatePrivateKey(2048)
 	const sk = key.toPrivatePem('base64')
 	const pk = key.toPublicPem('base64')
 	return { sk, pk }
