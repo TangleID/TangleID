04657b2a9062be486e174a15db41bab6ea78a2f0
HexRabbit <mikuqb8718@gmail.com>
2018-05-02 06:15:41 +0800

    Add temporary workaround to eliminate json encoding bugs

--- actions/createIdentity.js
+++ actions/createIdentity.js
@@ -6,15 +6,18 @@ import {
 	NEW_IDENTITY_SUCCESS,
 } from '../constants'
 
-const createNewIdentity = (params) => ({
+const createNewIdentity = (param) => ({
 	[CALL_API]: {
 		types: [ NEW_IDENTITY_REQUEST, NEW_IDENTITY_SUCCESS, NEW_IDENTITY_FAILURE ],
 		method: 'POST',
 		params: {
-			body: Object.assign({ command: 'new_user' }, transformParams(params))
+			//body: Object.assign({ command: 'new_user' }, transformParams(param))
+                  body: param
 		},
 		host: process.env.HOST_API,
-		endpoint: '/proxy/'
+		/* temporary workaround */
+		endpoint: '/createIdentity'
+		//endpoint: '/proxy/'
 	}
 })
 
