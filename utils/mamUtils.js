48553c072f9ac7185abc2aac4bee87d4cdc1903e
HexRabbit <mikuqb8718@gmail.com>
2018-05-23 14:38:37 +0800

    Replicate sender-side messages on Tangle

--- utils/mamUtils.js
+++ utils/mamUtils.js
@@ -14,262 +14,348 @@ var path = require('path')
 var main_path = path.dirname(require.main.filename)
 
 class mam {
-  /* make the callback function into promise */
-  constructor(callback) {
-    var self = this
+    /* make the callback function into promise */
+    constructor(callback) {
+        var self = this
 
-    var databaseInitialize = function() {
-      Mam.init(iota)
-      self.accountStore = new AccountStore(path.resolve(main_path, 'json', 'account.json'), () => {
-        self.contactStore = new ContactStore(path.resolve(main_path, 'json', 'contact.json'), () => {
-          self.messageStore = new MessageStore(path.resolve(main_path, 'json', 'message.json'), () => {
-            callback()
-          })
-        })
-      })
-    }
-    databaseInitialize()
-  }
-
-  async publishMessage(message, params) {
-
-    var packet = { msg: message }
-
-    /* add timestamp */
-    packet.timestamp = now.utc().unix()
-
-    var mamState = await this.getActiveMamState(params)
-    await this.sendMamMsg(packet, params, mamState)
-    this.messageUpdate(packet, params)
-  }
-
-  async fetchMessages(uuid) {
-
-    const recvPriv = this.accountStore.findOnly({id: uuid}).sk
-    const roots = await Cert.getBundles(uuid, 'M').then(data => {
-      return data.map(item => {
-        try {
-          var msg = JSON.parse(tools.decryptUTF(item.message, recvPriv))
-          /* TODO: signature verification */
-          //        if(tools.verify(msg.root, msg.signature, send_id))
-          return {
-            id: msg.id,
-            root: msg.root
-          }
-        } catch(err) {
-          console.log(err)
+        var databaseInitialize = function() {
+            Mam.init(iota)
+            self.accountStore = new AccountStore(path.resolve(main_path, 'json', 'account.json'), () => {
+                self.contactStore = new ContactStore(path.resolve(main_path, 'json', 'contact.json'), () => {
+                    self.messageStore = new MessageStore(path.resolve(main_path, 'json', 'message.json'), () => {
+                        callback()
+                    })
+                })
+            })
         }
-      })
-    })
-    console.log('Fetched roots:', roots)
-    var messages = {}
-    var contacts = this.contactStore.findOnly({id: uuid}).contacts
-
-    /* TODO: filter messages in contact */
-    //var contactRoots = roots.filter(msg => msg.id in contacts)
-    var contactRoots = roots
-
-    /* fetch from local */
-    var localmsg = this.messageStore.findOnly({id: uuid}).messages
-    //console.log('local:', localmsg)
-
-    await Promise.all(contactRoots.map(async tx => {
-      var id = tx.id
-      var root = tx.root
-      messages[id] = []
-
-      /* fetch from tangle */
-      await Mam.fetch(root, 'private', null, tryte => {
-        var data = JSON.parse(iota.utils.fromTrytes(tryte))
-        data.fromSelf = false
-        messages[id].push(data) /* data wrapped message and other things */
-      })
-
-    }))
-
-    Object.keys(localmsg).map(id => {
-
-      if (id in messages) {
-        /* sort by timestamp */
-        messages[id] = combineSortedArray(
-          messages[id], localmsg[id], (i, j) => {
-            return i.timestamp >= j.timestamp
-          }
-        )
-      }
-      else {
-        messages[id] = [].concat(localmsg[id])
-      }
-    })
-
-    return messages
-  }
-
-
-
-  async sendMamMsg(packet, params, mamState) {
-    console.log('sendMamMsg')
-
-    var recvPub = await Cert.getBundles(params.receiver, 'I')
-    if(recvPub.length !== 1)
-      console.log('error: Get Initial Claim failed')
-    recvPub = recvPub[0].message.pk
-
-    var sendPriv = this.accountStore.findOnly({id: params.sender}).sk
-
-    //  packet.signature = tools.signHash(packet.msg, sendPriv)
-    //  var msg = tools.encrypt(JSON.stringify(packet), recvPub)
-
-
-    var msg = JSON.stringify(packet)
-    var trytes = iota.utils.toTrytes(msg)
-    var message = Mam.create(mamState, trytes)
-
-    this.updateActiveMamState(params, message.state)
-
-    if(debug) {
-      console.log('Root: ', message.root)
-      console.log('Address: ', message.address)
-      console.log('length: ', message.payload.length)
+        databaseInitialize()
     }
 
-    var tx = await Mam.attach(message.payload, message.address)
-    console.log('sendMamMsg finished')
-  }
+    async publishMessage(message, params) {
 
-  addContact(self_id, add_id) {
-    var store = this.contactStore.findOnly({id: self_id})
-    if (add_id in store.contacts) {
-      console.log('[ERROR] error adding exist contact')
-      return
-    }
-    store.contacts[add_id] = {}
-    this.contactStore.update(store)
-  }
+        var packet = { msg: message }
 
-  async initMamClaim(params) {
-    console.log('initMamClaim')
-    //console.log(params)
-    var receiver_pub = await Cert.getBundles(params.receiver, 'I')
-    receiver_pub = receiver_pub[0].message.pk
-    //console.log('recvPub:', receiver_pub)
+        /* add timestamp */
+        packet.timestamp = now.utc().unix()
 
-    var sender_priv = this.accountStore.findOnly({id: params.sender}).sk
-    //console.log('sendPriv:', sender_priv)
+        var mamState = await this.getActiveMamState(params)
+        await this.sendMamMsg(packet, params, mamState)
 
-    const state = this.getInitMamState(params)
-    const root = Mam.getRoot(state)
-    //console.log(root)
-    var packet = {
-      id: params.sender,
-      root: root,
-      /* TODO: Signature is too large */
-      //    signature: tools.sign(root, sender_priv)
+        var bkState = await this.getBackupMamState(params)
+        await this.backupMamMsg(packet, params, bkState)
+
+        this.messageUpdate(packet, params)
     }
 
-    if(debug) {
-      console.log('id: ', params.sender)
-      console.log('root: ', root)
-      console.log('len:' + JSON.stringify(packet).length)
+    async getBackupMamState(params) {
+
+        var store = this.contactStore.findOnly({id: params.sender})
+
+        return deepCopy(store.contacts[params.receiver].backupMamState)
     }
 
-    var enc = tools.encrypt(JSON.stringify(packet), receiver_pub)
-    var tx = await Cert.attach(enc, params.receiver, 'M', null)
-  }
 
+    async recoverMamMsg(params) {
+        console.log('message recovering')
 
-  getInitMamState(params) {
-    console.log('getInitMamState')
+        var backupRoot = this.getBackupRoot(params)
+        var messages = []
 
-    var mamState = Mam.init(iota)
-    mamState = Mam.changeMode(mamState, 'private')
-    var msg = Mam.create(mamState, 'INITIALMESSAGE')
-    var newState = deepCopy(msg.state)
-    var store = this.contactStore.findOnly({id: params.sender})
+        await Mam.fetch(backupRoot, 'private', null, trytes => {
+            var data = JSON.parse(iota.utils.fromTrytes(tryte))
+            data.fromSelf = true
+            messages.push(data) /* data wrapped message and other things */
+        })
 
-    store.contacts[params.receiver] = {activeMamState: deepCopy(mamState)}
+        var store = this.messageStore.findOnly({id: params.sender})
+        store.messages[params.receiver] = [].concat(messages)
 
-    if(debug)
-      console.log('Store updated: ', store)
-
-    this.contactStore.update(store)
-
-    return newState
-  }
-
-  async getActiveMamState(params) {
-    console.log('getActiveMamState')
-
-    var store = this.contactStore.findOnly({id: params.sender})
-    //console.log(store)
-    var receiver = store.contacts[params.receiver]
-
-    if(!(params.receiver in store.contacts)) {
-      await this.initMamClaim(params)
-      store = this.contactStore.findOnly({id: params.sender})
-      receiver = store.contacts[params.receiver]
+        this.messageStore.update(store)
     }
 
-    return deepCopy(receiver.activeMamState)
-  }
+    getBackupRoot(params) {
 
-  updateActiveMamState(params, mamState) {
-    var store = this.contactStore.findOnly({id: params.sender})
-    store.contacts[params.receiver].activeMamState = deepCopy(mamState)
+      var store = this.contactStore.findOnly({id: params.sender})
 
-    this.contactStore.update(store)
-  }
+      return store.contacts[params.receiver].backupRoot
+    }
 
-  /* temporary workaround */
-  async createIdentity(packet) {
-    packet = deepCopy(packet)
-    delete packet.sk
-    await Cert.attach(packet, packet.uuid, 'I')
-  }
 
-  accountUpdate(account) {
-    this.accountStore.insert(account)
-    this.contactStore.insert({
-      id: account.id,
-      contacts: {}
-    })
-    this.messageStore.insert({
-      id: account.id,
-      messages: {}
-    })
-  }
+    async fetchMessages(uuid) {
 
-  messageUpdate(packet, params) {
-    var store = this.messageStore.findOnly({id: params.sender})
-    packet.fromSelf = true
+        const recvPriv = this.accountStore.findOnly({id: uuid}).sk
+        const roots = await Cert.getBundles(uuid, 'M').then(data => {
+            return data.map(item => {
+                try {
+                    var msg = JSON.parse(tools.decryptUTF(item.message, recvPriv))
+                    /* TODO: signature verification */
+                    //        if(tools.verify(msg.root, msg.signature, send_id))
+                    return {
+                        id: msg.id,
+                        root: msg.root
+                    }
+                } catch(err) {
+                    console.log(err)
+                }
+            })
+        })
+        console.log('Fetched roots:', roots)
+        var messages = {}
+        var contacts = this.contactStore.findOnly({id: uuid}).contacts
 
-    store.messages[params.receiver] = []
-    store.messages[params.receiver].push(packet)
+        /* TODO: filter messages in contact */
+        //var contactRoots = roots.filter(msg => msg.id in contacts)
+        var contactRoots = roots
 
-    this.messageStore.update(store)
-  }
+        /* fetch from local */
+        var localmsg = this.messageStore.findOnly({id: uuid}).messages
+        //console.log('local:', localmsg)
+
+        await Promise.all(contactRoots.map(async tx => {
+            var id = tx.id
+            var root = tx.root
+            messages[id] = []
+
+            /* fetch from tangle */
+            await Mam.fetch(root, 'private', null, tryte => {
+                var data = JSON.parse(iota.utils.fromTrytes(tryte))
+                data.fromSelf = false
+                messages[id].push(data) /* data wrapped message and other things */
+            })
+
+        }))
+
+        Object.keys(localmsg).map(id => {
+
+            if (id in messages) {
+                /* sort by timestamp */
+                messages[id] = combineSortedArray(
+                    messages[id], localmsg[id],
+                    (i, j) => {
+                        return i.timestamp >= j.timestamp
+                    }
+                )
+            }
+            else {
+                messages[id] = [].concat(localmsg[id])
+            }
+        })
+
+        return messages
+    }
+
+
+    async backupMamMsg(packet, params, bkState) {
+        console.log('backupMamMsg')
+
+        var sendPub = await Cert.getBundles(params.receiver, 'I')
+        if (sendPub.length !== 1)
+          console.log('error: Get initial claim failed')
+        sendPub = sendPub[0].message.pk
+
+        var msg = JSON.stringify(packet)
+        var trytes = iota.utils.toTrytes(msg)
+        var message = Mam.create(bkState, trytes)
+
+        this.updateBackupMamState(params, message.state)
+
+        if(debug) {
+            console.log('Root: ', message.root)
+            console.log('Address: ', message.address)
+            console.log('length: ', message.payload.length)
+        }
+
+        var tx = await Mam.attach(message.payload, message.address)
+        console.log('backupMamMsg finished')
+    }
+
+    async sendMamMsg(packet, params, mamState) {
+        console.log('sendMamMsg')
+
+        var recvPub = await Cert.getBundles(params.receiver, 'I')
+        if(recvPub.length !== 1)
+            console.log('error: Get initial claim failed')
+        recvPub = recvPub[0].message.pk
+
+        var sendPriv = this.accountStore.findOnly({id: params.sender}).sk
+
+        /* does private mode encrypt messages? */
+        //  var msg = tools.encrypt(JSON.stringify(packet), recvPub)
+
+
+        var msg = JSON.stringify(packet)
+        var trytes = iota.utils.toTrytes(msg)
+        var message = Mam.create(mamState, trytes)
+
+        this.updateActiveMamState(params, message.state)
+
+        if(debug) {
+            console.log('Root: ', message.root)
+            console.log('Address: ', message.address)
+            console.log('length: ', message.payload.length)
+        }
+
+        var tx = await Mam.attach(message.payload, message.address)
+        console.log('sendMamMsg finished')
+    }
+
+    addContact(self_id, add_id) {
+        var store = this.contactStore.findOnly({id: self_id})
+        if (add_id in store.contacts) {
+            console.log('[ERROR] error adding exist contact')
+            return
+        }
+        store.contacts[add_id] = {}
+        this.contactStore.update(store)
+    }
+
+    async initMamClaim(params) {
+        console.log('initMamClaim')
+        //console.log(params)
+        var receiver_pub = await Cert.getBundles(params.receiver, 'I')
+        receiver_pub = receiver_pub[0].message.pk
+        //console.log('recvPub:', receiver_pub)
+
+        var sender_priv = this.accountStore.findOnly({id: params.sender}).sk
+        //console.log('sendPriv:', sender_priv)
+
+        const state = this.getInitMamState(params)
+        const root = Mam.getRoot(state)
+        //console.log(root)
+        var packet = {
+            id: params.sender,
+            root: root,
+            /* TODO: Signature is too large */
+            //    signature: tools.sign(root, sender_priv)
+        }
+
+        if(debug) {
+            console.log('id: ', params.sender)
+            console.log('root: ', root)
+            console.log('len:' + JSON.stringify(packet).length)
+        }
+
+        var enc = tools.encrypt(JSON.stringify(packet), receiver_pub)
+        var tx = await Cert.attach(enc, params.receiver, 'M', null)
+    }
+
+
+    getInitMamState(params) {
+        console.log('getInitMamState')
+
+        var store = this.contactStore.findOnly({id: params.sender})
+
+        /* init mam state */
+        var mamState = Mam.init(iota)
+        mamState = Mam.changeMode(mamState, 'private')
+        /* in order to update next_root */
+        var msg = Mam.create(mamState, 'INITIALMESSAGE')
+        var newState = deepCopy(mamState)
+
+        store.contacts[params.receiver] = {
+          activeMamState: deepCopy(mamState)
+        }
+
+
+        /* init message backup mam state */
+        var bkState = Mam.init(iota)
+        bkState = Mam.changeMode(bkState, 'private')
+        var bkmsg = Mam.create(bkState, 'INITIALMESSAGE')
+
+        store.contacts[params.receiver].backupMamState = deepCopy(bkState)
+        store.contacts[params.receiver].backupRoot = bkState.next_root
+
+
+        if(debug)
+            console.log('Store updated: ', store)
+
+        this.contactStore.update(store)
+
+        return newState
+    }
+
+    async getActiveMamState(params) {
+        console.log('getActiveMamState')
+
+        var store = this.contactStore.findOnly({id: params.sender})
+        //console.log(store)
+        var receiver = store.contacts[params.receiver]
+
+        if(!(params.receiver in store.contacts)) {
+            await this.initMamClaim(params)
+            store = this.contactStore.findOnly({id: params.sender})
+            receiver = store.contacts[params.receiver]
+        }
+
+        return deepCopy(receiver.activeMamState)
+    }
+
+    updateActiveMamState(params, mamState) {
+        var store = this.contactStore.findOnly({id: params.sender})
+        store.contacts[params.receiver].activeMamState = deepCopy(mamState)
+
+        this.contactStore.update(store)
+    }
+
+    updateBackupMamState(params, bkState) {
+        var store = this.contactStore.findOnly({id: params.sender})
+        store.contacts[params.receiver].backupMamState = deepCopy(bkState)
+
+        this.contactStore.update(store)
+    }
+
+    /* temporary workaround */
+    async createIdentity(packet) {
+        packet = deepCopy(packet)
+        delete packet.sk
+        await Cert.attach(packet, packet.uuid, 'I')
+    }
+
+    accountUpdate(account) {
+        this.accountStore.insert(account)
+        this.contactStore.insert({
+            id: account.id,
+            contacts: {}
+        })
+        this.messageStore.insert({
+            id: account.id,
+            messages: {}
+        })
+    }
+
+    messageUpdate(packet, params) {
+        var store = this.messageStore.findOnly({id: params.sender})
+        packet.fromSelf = true
+
+        if (!params.receiver in messages) {
+            store.messages[params.receiver] = []
+        }
+        store.messages[params.receiver].push(packet)
+
+        this.messageStore.update(store)
+    }
 
 }
 function deepCopy(data) {
-  return JSON.parse(JSON.stringify(data))
+    return JSON.parse(JSON.stringify(data))
 }
 
 function combineSortedArray(arr1, arr2, cmp) {
-  var target = deepCopy(arr1)
-  var idx = 0
+    var target = deepCopy(arr1)
+    var idx = 0
 
-  for (var i = 0; i < arr2.length; idx++) {
+    for (var i = 0; i < arr2.length; idx++) {
 
-    if (idx === target.length) {
-      target = target.concat(arr2.slice(i))
-      break
+        if (idx === target.length) {
+            target = target.concat(arr2.slice(i))
+            break
+        }
+        if (cmp(target[idx], arr2[i])) {
+            target.splice(idx, 0, arr2[i])
+            i++
+        }
     }
-    if (cmp(target[idx], arr2[i])) {
-      target.splice(idx, 0, arr2[i])
-      i++
-    }
-  }
-  return target
+    return target
 }
 
 
