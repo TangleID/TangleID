3c11ca68d68f2f59b413b51e30ffd171ceba7f9f
leo <jamestsay1207@gmail.com>
2018-06-04 03:44:31 +0800

    Change user ui

--- pages/user.js
+++ pages/user.js
@@ -13,6 +13,73 @@ import SimpleForm from '../components/SimpleForm'
 import ClaimList from '../components/ClaimList'
 import Link from 'next/link'
 
+import CardActions from '@material-ui/core/CardActions';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardHeader from '@material-ui/core/CardHeader';
+
+import MessageInput from '../components/MessageInput'
+import Button from '@material-ui/core/Button'
+
+
+
+
+const styles = {
+	root: {
+		display: "flex",
+		["flex-flow"]: "row",
+		height: "75vh"
+	},
+	["left-block"]: {
+		["align-items"]: "center",
+		background: "#42A5F5",
+		display: "flex",
+		flex: "12%",
+		["flex-direction"]: "column",
+		["justify-content"]: "center"
+	},
+	["left-img"]: {
+		["text-align"]: "center"
+	},
+	["right-block"]: {
+		["align-items"]: "flex-start",
+		background: "#81D4FA",
+		display: "flex",
+		flex: "40%",
+		["justify-content"]: "flex-start"
+	},
+	qcode: {
+		height: "45%",
+		width: "45%"
+	},
+	["qcode-word"]:{
+		// color: "#000000",
+		color: "#424242",
+		["font-weight"]: "bolder",
+	},
+	["qcode-Name"]:{
+		// color: "#424242",
+		color: "#000000",
+		["font-size"]: "2.0em",
+		["font-weight"]: "bolder",
+	},
+	messageInput: {
+		display: "inline",
+		["margin-left"]: "1.2em"
+	},
+	Button_Coler:{
+		background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 40,
+		padding: '0 40px',
+		margin: 30,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
+	}
+}
+
 const UserPage = (props) => {
 	const { claims, user, createClaim } = props
 	const { pk, sk, id, claim, qrcode, } = user
@@ -28,37 +95,32 @@ const UserPage = (props) => {
 	}
 	return (
 		<Layout>
-			<h2>{claim.firstName}</h2>
-			<img src={qrcode} alt="QRCode of id and public key"/>
-			<div>
-				<p>id: {id}</p>
-				<p>public key: {pk}</p>
+			<div style={styles.root}>
+				<div style={styles["left-block"]}>
+					<div style={styles["qcode-Name"]}>Name</div>
+					<div style={styles["left-img"]}>
+						<p style={styles["qcode-word"]}>{claim.firstName}</p>
+						<img
+							alt="QRCode of id and public key"
+							style={styles.qcode}
+							src={qrcode} />
+					</div>
+				</div>
+				<div style={styles["right-block"]}>
+					<Card>
+						<CardHeader
+							title="Make Claim about User"
+						/>
+						<CardContent>
+							<span>Acting as: {claim.firstName}</span>
+							<MessageInput
+								placeholder="Message"
+								style={styles.messageInput} />
+							<Button style={styles.Button_Coler}>{`Sign & Attach`}</Button>
+						</CardContent>
+					</Card>
+				</div>
 			</div>
-			<div>
-				<p>private key: {sk}</p>
-			</div>
-			<div>
-				<h3>Claims about this user</h3>
-				{claims.length === 0 && (
-					<p>No Claims</p>
-				)}
-				{claims.length !== 0 && (
-					<ClaimList claims={claims}></ClaimList>
-				)}
-			</div>
-			<SimpleForm
-				name="signClaim"
-				handleSubmit={handleSubmit}
-				meta={{
-					inputs: [{
-						name: 'msg',
-						label: 'message',
-					}],
-					submit: {
-						text: 'Sign & Attach'
-					}
-				}}
-			/>
 		</Layout>
 	)
 }
