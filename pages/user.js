d14df81fbdf149896bff26fccd7f31c274acf9e9
ender503 <seph.serg@gmail.com>
2018-06-04 15:12:23 +0800

    Fix pages coding style

--- pages/user.js
+++ pages/user.js
@@ -1,24 +1,27 @@
-import { bindActionCreators } from 'redux'
-import withRedux from 'next-redux-wrapper'
-import configureStore from '../store/configureStore'
-import checkTangleUsers from '../actions/checkTangleUsers'
-//import fetchOffTangleUserList from '../actions/fetchOffTangleUserList'
-import fetchUserList from '../actions/fetchUserList'
-import fetchClaims from '../actions/fetchClaims'
-import fetchMamMessages from '../actions/fetchMamMessages'
-import createClaim from '../actions/createClaim'
-import createMamMessage from '../actions/createMamMessage'
-import transformToQRCode from '../utils/transformToQRCode'
-//import Layout from '../layouts/Main'
-import Layout from '../layouts/material/Main'
-import Button from '@material-ui/core/Button'
-import SimpleForm from '../components/SimpleForm'
-import ClaimList from '../components/ClaimList'
-import Link from 'next/link'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import withRedux from 'next-redux-wrapper';
+
+import Button from '@material-ui/core/Button';
+
+import configureStore from '../store/configureStore';
+import checkTangleUsers from '../actions/checkTangleUsers';
+import fetchUserList from '../actions/fetchUserList';
+import fetchClaims from '../actions/fetchClaims';
+import fetchMamMessages from '../actions/fetchMamMessages';
+import createClaim from '../actions/createClaim';
+import createMamMessage from '../actions/createMamMessage';
+import transformToQRCode from '../utils/transformToQRCode';
+import Layout from '../layouts/material/Main';
+import SimpleForm from '../components/SimpleForm';
+import ClaimList from '../components/ClaimList';
 
 const UserPage = (props) => {
-  const { claims, messages, user, otherUser, createClaim, createMamMessage } = props
-  const { pk, sk, id, claim, qrcode, } = user
+  const { claims, messages, user } = props;
+  const {
+    pk, sk, id, claim, qrcode,
+  } = user;
   const handleSubmit = (values) => {
     const params = Object.assign({
       uuid: id,
@@ -26,134 +29,146 @@ const UserPage = (props) => {
       partB: '',
       expDate: '',
       claimPic: '',
-    }, values)
-    createClaim(params)
-  }
+    }, values);
+    props.createClaim(params);
+  };
   const handleMamSubmit = (values) => {
     const params = {
       sender: localStorage.getItem('act_as_id'),
       receiver: id,
-      msg: values.msg
-    }
-    createMamMessage(params)
-  }
-  const handleSetUser = (values) => {
-    localStorage.setItem('act_as_id', id)
-  }
-	return (
-		<Layout>
-			<h2>{claim.firstName}</h2>
-			<img src={qrcode} alt="QRCode of id and public key"/>
-			<div>
-				<p>id: {id}</p>
-				<p>public key: {pk}</p>
-			</div>
-			<div>
-				<p>private key: {sk}</p>
-			</div>
-			<div>
-				<h3>Claims about this user</h3>
-				{claims.length === 0 && (
-					<p>No Claims</p>
-				)}
-				{claims.length !== 0 && (
-					<ClaimList claims={claims}></ClaimList>
-				)}
-			</div>
-			<div>
-				<h3>Mam Messages</h3>
-				{Object.keys(messages).length === 0 && (
-					<p>No Messages</p>
-				)}
-				{messages.length !== 0 && (
-                                  Object.keys(messages).map( (key, i) => (
-                                    <div key={'div-test-'+i}>
-                                    <ul key={'ul1-'+i}>
-                                      <li key={'li1-'+i}> {key} </li>
-                                      <ul key={'ul2-'+i}>
-                                    {messages[key].map((data, j) => (
-                                      <li key={'li3-'+i+j}>{data.msg} {data.fromSelf? '- You' : ''}</li>
-                                    ))}
-                                      </ul>
-                                    </ul>
-                                    </div>
-				)))}
-			</div>
-			<SimpleForm
-				name="signClaim"
-				handleSubmit={handleSubmit}
-				meta={{
-					inputs: [{
-						name: 'msg',
-						label: 'message',
-					}],
-					submit: {
-						text: 'Sign & Attach'
-					}
-				}}
-			/>
-			<SimpleForm
-				name="sendMessage"
-				handleSubmit={handleMamSubmit}
-				meta={{
-					inputs: [{
-						name: 'msg',
-						label: 'sendMamMessage',
-					}],
-					submit: {
-						text: 'Send'
-					}
-				}}
-			/>
+      msg: values.msg,
+    };
+    props.createMamMessage(params);
+  };
+  const handleSetUser = () => {
+    localStorage.setItem('act_as_id', id);
+  };
+  return (
+    <Layout>
+      <h2>{claim.firstName}</h2>
+      <img src={qrcode} alt="QRCode of id and public key" />
+      <div>
+        <p>id: {id}</p>
+        <p>public key: {pk}</p>
+      </div>
+      <div>
+        <p>private key: {sk}</p>
+      </div>
+      <div>
+        <h3>Claims about this user</h3>
+        {claims.length === 0 && (
+        <p>No Claims</p>
+        )}
+        {claims.length !== 0 && (
+        <ClaimList claims={claims} />
+        )}
+      </div>
+      <div>
+        <h3>Mam Messages</h3>
+        {Object.keys(messages).length === 0 && (
+        <p>No Messages</p>
+        )}
+        {messages.length !== 0 && (
+          Object.keys(messages).map(key => (
+            <div key={`div-test-${key}`}>
+              <ul>
+                <li> {key} </li>
+                <ul>
+                  {messages[key].map(data => (
+                    <li key={`li3-${key}${data.msg}`}>{data.msg} {data.fromSelf ? '- You' : ''}</li>
+                  ))}
+                </ul>
+              </ul>
+            </div>
+        )))}
+      </div>
+      <SimpleForm
+        name="signClaim"
+        handleSubmit={handleSubmit}
+        meta={{
+          inputs: [{
+            name: 'msg',
+            label: 'message',
+          }],
+          submit: {
+            text: 'Sign & Attach',
+          },
+        }}
+      />
+      <SimpleForm
+        name="sendMessage"
+        handleSubmit={handleMamSubmit}
+        meta={{
+          inputs: [{
+            name: 'msg',
+            label: 'sendMamMessage',
+          }],
+          submit: {
+            text: 'Send',
+          },
+        }}
+      />
 
-			<div style={{display: 'flex', justifyContent: 'center'}}>
-				<Button onClick={handleSetUser} type="submit" variant="raised" color="primary" size="small">
-                   SetUser
-                </Button>
-			</div>
-		</Layout>
-	)
-}
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button onClick={handleSetUser} type="submit" variant="raised" color="primary" size="small">
+          SetUser
+        </Button>
+      </div>
+    </Layout>
+  );
+};
+
+UserPage.propTypes = {
+  claims: PropTypes.array,
+  messages: PropTypes.object,
+  user: PropTypes.object,
+  createClaim: PropTypes.func,
+  createMamMessage: PropTypes.func,
+};
 
 UserPage.getInitialProps = async (context) => {
-	const { store } = context
-	const { id } = context.query
-//	await store.dispatch(fetchOffTangleUserList())
-        await store.dispatch(fetchUserList())
-        /* what is this used for ? */
-	await store.dispatch(checkTangleUsers([{ id }]))
-	await store.dispatch(fetchClaims(id))
-	await store.dispatch(fetchMamMessages(id))
-	const { users } = store.getState()
-	const { localList, validData, claims, messages } = users
-	/* not working at all */
-	const validIds = validData.map(v => v.id)
-	// TODO: Use selector to get better performance
-//	const userList = offTangleData.filter(d => validIds.indexOf(d.id) !== -1)
-	const userList = localList//.filter(d => validIds.indexOf(d.id) !== -1)
-	const user = userList.find(u => u.id === id)
-	const otherUser = userList.map(u => {return u.id}).filter(i => i !== id)
-	const { pk } = user
-	user.qrcode = await transformToQRCode(JSON.stringify({pk, id}))
-	return {
-		id,
-		user,
-		claims,
-		messages,
-		otherUser,
-	}
-}
+  const { store } = context;
+  const { id } = context.query;
+  //  await store.dispatch(fetchOffTangleUserList())
+  await store.dispatch(fetchUserList());
+  /* what is this used for ? */
+  await store.dispatch(checkTangleUsers([{ id }]));
+  await store.dispatch(fetchClaims(id));
+  await store.dispatch(fetchMamMessages(id));
+  const { users } = store.getState();
+  const {
+    localList, claims, messages,
+  } = users;
+  /* not working at all */
+  // const validIds = validData.map(v => v.id);
+  // TODO: Use selector to get better performance
+  //  const userList = offTangleData.filter(d => validIds.indexOf(d.id) !== -1)
+  const userList = localList;// .filter(d => validIds.indexOf(d.id) !== -1)
+  const user = userList.find(u => u.id === id);
+  const otherUser = userList.map(u => u.id).filter(i => i !== id);
+  const { pk } = user;
+  user.qrcode = await transformToQRCode(JSON.stringify({ pk, id }));
+  return {
+    id,
+    user,
+    claims,
+    messages,
+    otherUser,
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-	return {
-		createClaim: bindActionCreators(createClaim, dispatch),
-		createMamMessage: bindActionCreators(createMamMessage, dispatch)
-	}
-}
+const mapDispatchToProps = dispatch => ({
+  createClaim: bindActionCreators(createClaim, dispatch),
+  createMamMessage: bindActionCreators(createMamMessage, dispatch),
+});
 
 const mapStateToProps = (state, ownProps) => {
-	const { user, claims, messages, otherUser } = ownProps
-	return { user, claims, messages, otherUser }
-}
+  const {
+    user, claims, messages, otherUser,
+  } = ownProps;
+  return {
+    user, claims, messages, otherUser,
+  };
+};
 
-export default withRedux(configureStore, mapStateToProps, mapDispatchToProps)(UserPage)
+export default withRedux(configureStore, mapStateToProps, mapDispatchToProps)(UserPage);
--- pages/users/new.js
+++ pages/users/new.js
@@ -1,65 +1,70 @@
-import { bindActionCreators } from 'redux'
-import withRedux from 'next-redux-wrapper'
-import configureStore from '../../store/configureStore'
-import fetchKeyPairs from '../../actions/fetchKeyPairs'
-import createNewIdentity from '../../actions/createIdentity'
-import updateLocalAccount from '../../actions/updateLocalAccount'
-//import Layout from '../../layouts/Main'
-import Layout from '../../layouts/material/Main'
-import NewUserForm from '../../components/NewUserForm'
-import LoginDialog from '../../components/LoginDialog'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import withRedux from 'next-redux-wrapper';
 
-const gen_uuid = () => {
-	const validChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9'
-	var ret = ''
-	for (var i = 0; i < 26; ++i)
-		ret = ret.concat(validChar[Math.floor(Math.random() * 27)])
-	return ret
-}
+import configureStore from '../../store/configureStore';
+import fetchKeyPairs from '../../actions/fetchKeyPairs';
+import createNewIdentity from '../../actions/createIdentity';
+import updateLocalAccount from '../../actions/updateLocalAccount';
+import Layout from '../../layouts/material/Main';
+import NewUserForm from '../../components/NewUserForm';
+
+const generateUUID = () => {
+  const validChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9';
+  let ret = '';
+  for (let i = 0; i < 26; i += 1) { ret = ret.concat(validChar[Math.floor(Math.random() * 27)]); }
+  return ret;
+};
 
 const NewUserPage = (props) => {
-	const { keyPairs, isLoading, createNewIdentity, updateLocalAccount } = props
-	const { sk, skImg, pk, pkImg, } = keyPairs
-	const uuid = gen_uuid()
-	const handleSubmit = (values) => {
-          const signature = ""
-          const params = Object.assign({ sk, pk, uuid }, values)
-          createNewIdentity(params)
-          var account = { claim: values, id: uuid, sk, pk }
-          updateLocalAccount(account)
-        }
-	return (
-		<Layout>
-			<div style={{display: 'flex', justifyContent: 'center'}}>
-				<NewUserForm
-					keyPairs={keyPairs}
-					uuid={uuid}
-					handleSubmit={handleSubmit} />
-			</div>
-		</Layout>
-	)
-}
+  const { sk, pk } = props.keyPairs;
+  const uuid = generateUUID();
+  const handleSubmit = (values) => {
+    const params = Object.assign({ sk, pk, uuid }, values);
+    props.createNewIdentity(params);
+    const account = {
+      claim: values, id: uuid, sk, pk,
+    };
+    props.updateLocalAccount(account);
+  };
+  return (
+    <Layout>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <NewUserForm
+          keyPairs={props.keyPairs}
+          uuid={uuid}
+          handleSubmit={handleSubmit}
+        />
+      </div>
+    </Layout>
+  );
+};
+
+NewUserPage.propTypes = {
+  keyPairs: PropTypes.object,
+  createNewIdentity: PropTypes.func,
+  updateLocalAccount: PropTypes.func,
+};
 
 NewUserPage.getInitialProps = async (context) => {
-	const { store, isServer } = context
-	await store.dispatch(fetchKeyPairs())
-	return { isServer }
-}
+  const { store, isServer } = context;
+  await store.dispatch(fetchKeyPairs());
+  return { isServer };
+};
 
-const mapDispatchToProps = (dispatch) => {
-	return {
-		createNewIdentity: bindActionCreators(createNewIdentity, dispatch),
-    updateLocalAccount: bindActionCreators(updateLocalAccount, dispatch),
-	}
-}
+const mapDispatchToProps = dispatch => ({
+  createNewIdentity: bindActionCreators(createNewIdentity, dispatch),
+  updateLocalAccount: bindActionCreators(updateLocalAccount, dispatch),
+});
 
 const mapStateToProps = (state) => {
-	const { keyPairs, isLoading } = state.users
-	return {
-		isLoading,
-		keyPairs,
-	}
-}
+  const { keyPairs, isLoading } = state.users;
+  return {
+    isLoading,
+    keyPairs,
+  };
+};
 
 
-export default withRedux(configureStore, mapStateToProps, mapDispatchToProps)(NewUserPage)
+export default withRedux(configureStore, mapStateToProps, mapDispatchToProps)(NewUserPage);
