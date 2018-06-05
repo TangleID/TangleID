2d5c07551c0d04a893a1133f56c61fee799a6e5b
ender503 <seph.serg@gmail.com>
2018-06-05 10:58:15 +0800

    Fix middlewares coding style

--- middlewares/api.js
+++ middlewares/api.js
@@ -1,74 +1,72 @@
-import { CALL_API } from '../constants'
-import fetchData from './fetchData'
+import { CALL_API } from '../constants';
+import fetchData from './fetchData';
 
-export default store => next => action => {
-	const callAPI = action[CALL_API]
-	if (typeof callAPI === 'undefined') {
-		return next(action)
-	}
+export default store => next => (action) => { // eslint-disable-line no-unused-vars
+  const callAPI = action[CALL_API];
+  if (typeof callAPI === 'undefined') {
+    return next(action);
+  }
 
-	let { endpoint } = callAPI
+  const { endpoint } = callAPI;
 
-	const {
-		types, params, method, host,
-		aggregate, getParams,
-		labelFunc,
-	} = callAPI
+  const {
+    types, params, method, host,
+    aggregate, getParams,
+    labelFunc,
+  } = callAPI;
 
-	if (typeof endpoint !== 'string') {
-		throw new Error('Specify a string endpoint URL.')
-	}
+  if (typeof endpoint !== 'string') {
+    throw new Error('Specify a string endpoint URL.');
+  }
 
-	if (!Array.isArray(types) || types.length !== 3) {
-		throw new Error('Expected an array of three action types: [REQUEST, SUCCESS, FAILURE]')
-	}
-	if (!types.every(type => typeof type === 'string')) {
-		throw new Error('Expected action types to be strings.')
-	}
+  if (!Array.isArray(types) || types.length !== 3) {
+    throw new Error('Expected an array of three action types: [REQUEST, SUCCESS, FAILURE]');
+  }
+  if (!types.every(type => typeof type === 'string')) {
+    throw new Error('Expected action types to be strings.');
+  }
 
-	const actionWith = data => {
-		const finalAction = Object.assign({}, action, data)
-		delete finalAction[CALL_API]
-		return finalAction
-	}
+  const actionWith = (data) => {
+    const finalAction = Object.assign({}, action, data);
+    delete finalAction[CALL_API];
+    return finalAction;
+  };
 
-	const [ requestType, successType, failureType ] = types
-	next(actionWith({ type: requestType }))
+  const [requestType, successType, failureType] = types;
+  next(actionWith({ type: requestType }));
 
 
-	let promise
+  let promise;
 
-	function getPromise(p) {
-		if (!p) {
-			return fetchData(host, endpoint, 'GET')
-		}
-		if (labelFunc) {
-			const label = labelFunc(p)
-			return fetchData(host, endpoint, method, p.body, label)
-		}
-		return fetchData(host, endpoint, method, p.body)
-	}
+  function getPromise(p) {
+    if (!p) {
+      return fetchData(host, endpoint, 'GET');
+    }
+    if (labelFunc) {
+      const label = labelFunc(p);
+      return fetchData(host, endpoint, method, p.body, label);
+    }
+    return fetchData(host, endpoint, method, p.body);
+  }
 
-	if (aggregate) {
-		const paramsList = getParams()
-		promise = Promise.all(
-			paramsList.map(getPromise)
-		)
-	} else {
-		promise = getPromise(params)
-	}
+  if (aggregate) {
+    const paramsList = getParams();
+    promise = Promise.all(paramsList.map(getPromise));
+  } else {
+    promise = getPromise(params);
+  }
 
-	return promise.then(
-		response => {
-			next(actionWith({
-				response,
-				type: successType
-			}))
-		},
-		error => next(actionWith({
-			type: failureType,
-			error: error.message || 'Something bad happened'
-		}))
-	)
-}
+  return promise.then(
+    (response) => {
+      next(actionWith({
+        response,
+        type: successType,
+      }));
+    },
+    error => next(actionWith({
+      type: failureType,
+      error: error.message || 'Something bad happened',
+    })),
+  );
+};
 
