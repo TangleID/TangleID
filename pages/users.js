e9c647122ff14b459e17bcf9bedf8b0e335ab149
LEO <jamestsay1207@gmail.com>
2018-06-09 22:48:25 +0800

    Fix Users coding style

--- pages/users.js
+++ pages/users.js
@@ -1,41 +1,43 @@
-import withRedux from 'next-redux-wrapper'
-import configureStore from '../store/configureStore'
-//import fetchOffTangleUserList from '../actions/fetchOffTangleUserList'
-import checkTangleUsers from '../actions/checkTangleUsers'
-import fetchUserList from '../actions/fetchUserList'
-//import Layout from '../layouts/Main'
-import Layout from '../layouts/material/Main'
-import UserList from '../components/UserList'
+import React from 'react';
+import withRedux from 'next-redux-wrapper';
+import configureStore from '../store/configureStore';
+// import fetchOffTangleUserList from '../actions/fetchOffTangleUserList'
+import checkTangleUsers from '../actions/checkTangleUsers';
+import fetchUserList from '../actions/fetchUserList';
+// import Layout from '../layouts/Main'
+import Layout from '../layouts/material/Main';
+import UserList from '../components/UserList';
 
 const UsersPage = (props) => {
-	const { userList } = props
-	return (
-		<Layout>
-			<UserList users={userList}></UserList>
-		</Layout>
-	)
-}
+  const { userList } = props;
+  return (
+    <Layout>
+      <UserList users={userList} />
+    </Layout>
+  );
+};
 
 UsersPage.getInitialProps = async (context) => {
-	const { isServer, store } = context
-//	await store.dispatch(fetchOffTangleUserList())
-  await store.dispatch(fetchUserList())
-	const { localList } = (store.getState()).users
-	// await store.dispatch(checkTangleUsers(offTangleData.slice(0, 2)))
-	await store.dispatch(checkTangleUsers(localList))
-	return { isServer }
-}
+  const { isServer, store } = context;
+  //  await store.dispatch(fetchOffTangleUserList())
+  await store.dispatch(fetchUserList());
+  const { localList } = (store.getState()).users;
+  // await store.dispatch(checkTangleUsers(offTangleData.slice(0, 2)))
+  await store.dispatch(checkTangleUsers(localList));
+  return { isServer };
+};
 
 const mapStateToProps = (state) => {
-	const { users } = state
-	const { localList, validData } = users
-	const validIds = validData.map(v => v.id)
-	// TODO: Use selector to get better performance
-	// const userList = offTangleData.filter(d => validIds.indexOf(d.id) !== -1)
-	const userList = localList.filter(d => validIds.indexOf(d.id) !== -1)
-//  console.log(localList)
-	return { userList }
-}
+  const { users } = state;
+  const { localList, validData } = users;
+  const validIds = validData.map(v => v.id);
+  // TODO: Use selector to get better performance
+  // const userList = offTangleData.filter(d => validIds.indexOf(d.id) !== -1)
+  const userList = localList.filter(d => validIds.indexOf(d.id) !== -1);
+  //  console.log(localList)
+  return { userList };
+};
 
-export default withRedux(configureStore, mapStateToProps)(UsersPage)
+
+export default withRedux(configureStore, mapStateToProps)(UsersPage);
 
