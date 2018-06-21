506346a8433b708567ea0b94a5fffb7ee0dcd149
ender503 <ender503.tw@gmail.com>
2018-06-21 18:40:26 +0800

    Shorten user profile page load time
    The user profile page takes long time to load each page, that was caused
    by fetching too much data in the server-side with "getInitailProps"
    function.
    
    By moving API calls from "getInitialProps" into "componentDidMount",
    the server will respond faster, then call API after the page is loaded.
    
    See also #38

--- components/ClaimList/index.js
+++ components/ClaimList/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
-function ClaimList(props) {
-  return (
-    <div>
-      {props.claims.map(claim => (
-        <div key={`claim-${claim}`}>
-          <Link href={`/claims/info/?hash_txn=${JSON.stringify(claim)}`} as={`/claims/info/${claim}`}>
-            <a>{claim}</a>
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
+class ClaimList extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.claims.map(claim => (
+          <div key={`claim-${claim}`}>
+            <Link href={`/claims/info/?hash_txn=${JSON.stringify(claim)}`} as={`/claims/info/${claim}`}>
+              <a>{claim}</a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 ClaimList.propTypes = {
---
+++ components/MessageList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <div>
+        {
+          Object.keys(this.props.messages).map(key => (
+            <div key={`div-test-${key}`}>
+              <ul>
+                <li> {key} </li>
+                <ul>
+                  {this.props.messages[key].map(data => (
+                    <li key={`li3-${key}${data.msg}`}>{data.msg} {data.fromSelf ? '- You' : ''}</li>
+                  ))}
+                </ul>
+              </ul>
+            </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+MessageList.propTypes = {
+  messages: PropTypes.array,
+};
+
+
+export default MessageList;
