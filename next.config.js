7296e7b1842341a798142b9928abc8500e602256
ender503 <ender503.tw@gmail.com>
2018-07-02 18:46:42 +0800

    Preserve default environment values
    Since we are using the '.env' file to set the environment values, it is
    possible to start up the website failed when the user not set up the
    '.env' file correctly.
    
    By preserving the default environment values, it could reduce the
    change to start up the website failed.
    
    It is weird to load the environment values in the Next.js configuration,
    we should load the environment in the 'server.js' when we remove the
    Next.js.

--- next.config.js
+++ next.config.js
@@ -3,9 +3,19 @@ const webpack = require('webpack');
 
 const customEnvironments = dotenv.config().parsed;
 
+const defaultEnvironments = {
+  BACKEND: 'http://node2.puyuma.org:8000',
+  API_HOST: 'http://localhost:3000/api',
+};
+
+// Merge with default environments
+const environments = Object.assign(defaultEnvironments, customEnvironments);
+// Set environments to 'process.env'
+process.env = Object.assign(process.env, environments);
+
 module.exports = {
   webpack: (config) => {
-    const environmentPlugin = new webpack.EnvironmentPlugin(customEnvironments);
+    const environmentPlugin = new webpack.EnvironmentPlugin(environments);
     config.plugins.push(environmentPlugin);
 
     return config;
