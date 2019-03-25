33c2e76f59ecca14b4f606488d261a7227b8edd5
ender503 <ender503.tw@gmail.com>
2019-03-25 10:15:49 +0800

    Fix mam client not mocked
    In order to test the MAM without interaction with the real iota node,
    we use the mam.tools.js to mock the MAM client, but mocking mam client
    was failed.
    
    To fix this problem, set the virtual option and add the mam.client.js
    into package dependencies.

--- packages/did/test/did.test.js
+++ packages/did/test/did.test.js
@@ -2,9 +2,13 @@ const mock = require('mam.tools.js/test/mamMock');
 
 const { DIDUtils } = require('../src/index');
 
-jest.mock('mam.client.js', () => {
-  return mock;
-});
+jest.mock(
+  'mam.client.js',
+  () => {
+    return mock;
+  },
+  { virtual: true }
+);
 
 describe('Register and Resolve DID Document', () => {
   it('Create DID document', async () => {
