f3e0deffc7446d5aa1da7261f76ba449acf4c14d
ender503 <ender503.tw@gmail.com>
2019-06-27 07:43:53 +0800

    fix(all): Fix generate unnecessary code
    Using the path mapping feature in the "tsconfig.json" will cause
    TypeScript compiler treats these package as source files and compile
    them to generate unnecessary JavaScript code.
    
    We need the path mapping feature to run the test cases without
    compiling any TypeScript source code, so split the "tsconfig.json"
    for different usages.
    
     - "tsconfig.json" compiling the source code (w/o path mapping)
     - "tsconfig.test.json" running the test case (w path mapping)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig');
+const { compilerOptions } = require('./tsconfig.test');
 
 module.exports = {
   preset: 'ts-jest',
